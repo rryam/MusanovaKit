c4a1ba7d34db67fce4678801edee8b5d6ed071d3
Rudrank Riyam <rudrankriyam@gmail.com>
2023-04-04 21:56:18 +0530

    Add Musanova app

---
+++ Musanova/Musanova/MusanovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusanovaApp.swift
+//  Musanova
+//
+//  Created by Rudrank Riyam on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MusanovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
